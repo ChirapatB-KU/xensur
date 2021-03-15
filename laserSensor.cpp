364040edf05296ef84226bf16fd441e472308536
KK44689 <kirima111.04@hotmail.com>
2021-03-15 22:27:00 +0700

    add master writer code

--- laserSensor.cpp
+++ laserSensor.cpp
@@ -1,11 +1,17 @@
-int sensor = 35;
-int led = 34;
+#include <Wire.h>
+int sensor1 = 35;
+int led = 2;
+//buzzer = ;
+
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);
   pinMode(led,OUTPUT);
+  //pinMode(buzzer,OUTPUT);
   pinMode(sensor,INPUT);
 }
+byte x = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -13,8 +19,14 @@ void loop() {
   boolean val = digitalRead(sensor);
   if(!val){
     digitalWrite(led,HIGH);
+    Wire.beginTransmission(4); // transmit to device #4
+    Wire.write(1);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    //tone(buzzer,450);delay(500);
+    //noTone(buzzer);
   }else{
     digitalWrite(led,LOW);
+    //noTone(buzzer);
   }
   delay(10);
   Serial.println(val);
