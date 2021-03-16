a2879abc69e63a25c5ddd4e346f396e274bb713c
Chayanich <chayanich109.6@gmail.com>
2021-03-16 15:04:11 +0700

    10digit func

--- bread10digit/bread10digit.ino
+++ bread10digit/bread10digit.ino
@@ -1,11 +1,33 @@
-const int ledCount = 10; // the number of LEDs in the bar graph
- 
-int ledPins[] = {
-32, 33, 34, 35, 25, 26, 27, 14,12,13
-}; // an array of pin numbers to which LEDs are attached
- 
-void setup()
-{
+const int ledCount = 6; // the number of LEDs in the bar graph
+//const int buttonPin = 8; 
+//int buttonState = 0; 
+
+int ledPins[] = {32, 33, 25, 27, 14,12}; 
+
+void digit_setup(){
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
+       digitalWrite(ledPins[thisLed], HIGH);
+       delay(500);
+       digitalWrite(ledPins[thisLed], LOW);
+  }
+}
+
+void digit_play(){
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
+    digitalWrite(ledPins[thisLed], HIGH);
+  }
+  for (int thisLed = 0; thisLed < ledCount; thisLed++)
+  {
+    delay(10000);
+    digitalWrite(ledPins[thisLed], LOW);
+  }
+}
+
+
+
+void setup() {
   // loop over the pin array and set them all to output and high
   for (int thisLed = 0; thisLed < ledCount; thisLed++)
   {
@@ -13,20 +35,15 @@ void setup()
     digitalWrite(ledPins[thisLed], HIGH);
   }
 }
- 
-void loop()
-{
- 
-  // loop over the LED array:
-  for (int thisLed = 0; thisLed <= ledCount; thisLed++)
-  {
-    digitalWrite(ledPins[thisLed], LOW);
-    delay(500);
-  }
- 
-  for (int thisLed = 0; thisLed < ledCount; thisLed++)
-  {
-    digitalWrite(ledPins[thisLed], HIGH);
-    delay(500);
-  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digit_setup();
+  delay(5000);
+  digit_play();
+  delay(500);
+  digit_setup();
+  delay(10000);
+
 }
