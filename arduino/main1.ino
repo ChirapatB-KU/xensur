67b8116e902b79aea36dd018d20b3a05ac0a57b2
KK44689 <kirima111.04@hotmail.com>
2021-03-30 13:14:16 +0700

    isolate reconnect wifi from setup

--- arduino/main1.ino
+++ arduino/main1.ino
@@ -226,13 +226,47 @@ void reconnect() {
   }
 }
 
+void reconnectWifi(){
+  // led on board
+  int ledPin = 2;
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  // connect to wifi
+  const char* ssid = "xensurnetwork";
+  const char* pass = "helloworld";
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  Serial.print("Connecting to WiFi");
+
+  while(WiFi.status()!=WL_CONNECTED){
+    digitalWrite(ledPin, HIGH);
+    delay(250);
+    digitalWrite(ledPin, LOW);
+    delay(250);
+    Serial.print("...");
+  }
+
+  //connected
+  for(int i=0; i<3; i++){
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
+  
+  Serial.println("\nConnected to the WiFi network");
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   if(!client.connected()){
     reconnect();  
   }
   if(WiFi.status()!=WL_CONNECTED){
-    setup();  
+    reconnectWifi();  
   }
   client.loop();
 
